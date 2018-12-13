86fc74b996267ab94fd491734df2ff8a4fd8aa35
Chris Liu <chris.liu@mycom-osi.com>
2018-12-13 15:34:31 +0800

    Added biFunction test case.

--- src/test/java/org/javasafari/function/BiFunctionTest.java
+++ src/test/java/org/javasafari/function/BiFunctionTest.java
@@ -1,9 +1,17 @@
 package org.javasafari.function;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.BiFunction;
+import java.util.function.Predicate;
+import java.util.stream.Collectors;
 
+import org.hamcrest.MatcherAssert;
 import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
+
+import static org.hamcrest.CoreMatchers.hasItems;
+
 
 /**
  * Represents a function that accepts two arguments and produces a result.
@@ -21,4 +29,25 @@ public class BiFunctionTest
       //verify
       Assertions.assertEquals("hello world", function1.apply("hello ", "world"));
    }
+
+
+   @Test
+   public void testFindEvenNumbers()
+   {
+
+      //given
+      BiFunction<List<Integer>, Predicate<Integer>, List<Integer>> evenFinder = (list, even) -> {
+         return list.stream().filter(even).collect(Collectors.toList());
+      };
+
+      //when
+      List<Integer> evenNumList = evenFinder.apply(
+         Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8),
+         (num) -> num % 2 == 0
+      );
+
+      //verify
+      MatcherAssert.assertThat(evenNumList, hasItems(2, 4, 6, 8));
+   }
+
 }
