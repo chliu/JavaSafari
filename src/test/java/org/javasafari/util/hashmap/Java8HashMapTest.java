9cef99e593389c7878825e85e6f30de85e51dc85
Chris Liu <chris.liu@mycom-osi.com>
2018-12-23 11:04:10 +0800

    Updated test cases for Map interface with new methods.

--- src/test/java/org/javasafari/util/hashmap/Java8HashMapTest.java
+++ src/test/java/org/javasafari/util/hashmap/Java8HashMapTest.java
@@ -6,50 +6,58 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-import org.junit.Before;
+import org.junit.Assert;
 import org.junit.Test;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasEntry;
+import static org.hamcrest.Matchers.hasItems;
+import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 
 public class Java8HashMapTest
 {
 
-   //given
-   Map<String, Integer> collector = new HashMap();
-
-   @Before
-   public void setUp()
-   {
-      String text = "All algorithms are built with parameters created by humans. All algorithms are built with ";
-      //when
-      for (String word : text.split(" "))
-      {
-         collector.computeIfAbsent(word, (key) -> 0);
-         collector.compute(word, (key, value) -> ++value);
-      }
-
-   }
-
-
    @Test
    public void testGetOfDefault()
    {
-      assertEquals(Integer.valueOf(0), collector.getOrDefault("ABC", 0));
+      Map<String, Integer> studentGrades = new HashMap<>();
+      assertThat(studentGrades.getOrDefault("Chris", 0), equalTo(new Integer(0)));
    }
 
 
    @Test
    public void testForEach()
    {
-      collector.forEach((key, value) -> System.out.println(String.format("%s#%s", key, value)));
+      Map<String, Integer> collector = new HashMap()
+      {
+         {
+            put("A", 99);
+            put("B", 80);
+            put("C", 70);
+         }
+      };
+      collector.forEach((key, value) -> System.out.println(String.format("%s => %s", key, value)));
    }
 
 
    @Test
    public void testReplaceAll()
    {
-      collector.replaceAll((key, value) -> value * value);
+      Map<String, Integer> collector = new HashMap()
+      {
+         {
+            put("A", 99);
+            put("B", 82);
+            put("C", 71);
+         }
+      };
+      collector.replaceAll((key, value) -> value < 90 ? value + 10 : value);
+      assertThat(collector, hasEntry("A", 99));
+      assertThat(collector, hasEntry("B", 92));
+      assertThat(collector, hasEntry("C", 81));
    }
 
 
@@ -86,6 +94,16 @@ public class Java8HashMapTest
    }
 
    @Test
+   public void testMergeValue()
+   {
+      Map<String, String> holder = new HashMap();
+      holder.put("chris", "^^~");
+      holder.merge("chris", "Java", (v1, v2) -> v1.concat(v2));
+      Assert.assertThat(holder.get("chris"), is("^^~Java"));
+   }
+
+
+   @Test
    public void testComputeIfPresentAndAbsent()
    {
 
@@ -95,9 +113,9 @@ public class Java8HashMapTest
       //when
       counters.computeIfAbsent("c1", (n) -> 0);
       counters.computeIfAbsent("c2", (n) -> 2);
+
+
       //when
-
-
       counters.computeIfPresent("c1", (k, v) -> ++v);
       counters.computeIfPresent("c2", (k, v) -> ++v);
 
@@ -107,27 +125,39 @@ public class Java8HashMapTest
 
    }
 
+   @Test
+   public void testComputeIfPresent()
+   {
+      Map<String, Integer> map = new HashMap()
+      {{
+         put("key", 2);
+      }};
+      map.computeIfPresent("key", (k, v) -> v / 2);
+      assertThat(map.get("key"), equalTo(1));
+   }
+
+
+   @Test
+   public void testComputeIfAbsent()
+   {
+      Map<String, List<Integer>> counters = new HashMap<>();
+      counters.computeIfAbsent("key", k -> new ArrayList<>()).addAll(Arrays.asList(1, 3, 5));
+      assertThat(counters.get("key"), hasItems(1, 3, 5));
+   }
+
 
    @Test
    public void testPutIfAbsent()
    {
+      Map<String, Integer> counters = new HashMap()
+      {{ put("c0", 0); }};
 
-      //given
-      Map<String, Integer> counters = new HashMap<>();
-      counters.put("c0", null);
-      counters.put("c1", 1);
-      counters.putIfAbsent("c2", 99);
-
-      //when
       counters.putIfAbsent("c0", 99);
       counters.putIfAbsent("c1", 99);
 
-      //verify
-      assertEquals(Integer.valueOf(99), counters.get("c0"));
-      assertEquals(Integer.valueOf(1), counters.get("c1"));
-      assertEquals(Integer.valueOf(99), counters.get("c2"));
-
+      assertEquals(Integer.valueOf(0), counters.get("c0"));
+      assertEquals(Integer.valueOf(99), counters.get("c1"));
    }
 
-
 }
+
